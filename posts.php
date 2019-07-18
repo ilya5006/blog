4336adb9966f48bd5581b49c593513025fc4d65e
ilya5006 <samarcev.ilya@mail.ru>
2019-07-19 01:55:23 +0300

    delete one post

--- posts.php
+++ posts.php
@@ -18,11 +18,12 @@
 
     <div id="posts">
         <?
-        $postsInfoQuery = "SELECT * FROM posts";
+        $postsInfoQuery = "SELECT * FROM posts ORDER BY id_post DESC";
         $postsInfoResultQuery = mysqli_query($link, $postsInfoQuery);
 
         while($postsInfoResult = mysqli_fetch_assoc($postsInfoResultQuery))
         {
+            $postId = $postsInfoResult['id_post'];
             $postName = $postsInfoResult['name'];
             $postTags = $postsInfoResult['tags'];
             $postText = $postsInfoResult['text'];
@@ -30,7 +31,33 @@
             $postImage = $postsInfoResult['image'];
 
             echo "<div id='post'>";
-                echo "<h2 id='postName'>$postName</h2>";
+                echo "<h2 id='postName'><a id='postNameLink' href=''>$postName</a></h2>";
+                ?>
+                <script>
+                    let postName = document.querySelector("#postNameLink");
+                    postName.addEventListener("click", function(event)
+                    {
+                        event.preventDefault();
+                    });
+                </script>
+                <?
+                if ($_SESSION['id_user'] == 1)
+                {
+                    echo "<form method='POST' id='deletePostForm'>";
+                        echo "<input type='submit' id='deletePostIcon' name='deletePost' value='$postId'>";
+                        $deletePostButton = $_POST['deletePost'];
+
+                        if (isset($deletePostButton) && $postId == $deletePostButton)
+                        {
+                            $imagePath = __DIR__ . "/post_images/" . $postImage;
+                            unlink($imagePath);
+
+                            $deletePostQuery = "DELETE FROM posts WHERE id_post = '$postId'";
+                            mysqli_query($link, $deletePostQuery);
+                            echo "<meta http-equiv='refresh' content='0'>";
+                        }
+                    echo "</form>";
+                }
                 echo "<img id='postImage' src='post_images/$postImage'>";
                 echo "<p id='postDate'>$postDate</p>";
                 echo "<p id='postTags'>$postTags</p>";
